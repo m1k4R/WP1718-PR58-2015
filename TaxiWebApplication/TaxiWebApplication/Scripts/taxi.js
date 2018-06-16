b27bb778ddfc3e2ccff56a67b6a17a6f4cf8571c
m1k4R <39298537+m1k4R@users.noreply.github.com>
2018-06-16 04:23:13 +0200

    Dodat izgled za logovanje

---
+++ TaxiWebApplication/TaxiWebApplication/Scripts/taxi.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    $("#loginButton").click(function() {
+        $("#myAccountButton").show();
+        $("#loginPage").show();
+        $("#loginButton").text("Logout");
+    });
+    $("#myAccountButton").click(function () {
+        $("#userBox").show();
+        $("#imgTriangle").show();
+        $("#homeText").hide();
+    });
+    $("#imgX").click(function () {
+        $("#loginPage").hide();
+    });
+    $("#homeButton").click(function () {
+        $("#userBox").hide();
+        $("#imgTriangle").hide();
+        $("#homeText").show();
+    });
+    var pos = 46;
+    $("#imgTriangle").click(function () {
+        if (pos === 46) {
+            $("#imgTriangle").animate({
+                top: '73vh'
+            });
+            pos = 73;
+        }
+        else {
+            $("#imgTriangle").animate({
+                top: '46vh'
+            });
+            pos = 46;
+        }
+        $("#userMenu").slideToggle();
+    });
+    $("#imgTriangleDown").click(function () {
+        $("#userMenu").slideUp();
+        $("#imgTriangle").show();
+    });
+});
